477d5d38a16860e5fdccc26ce5c0f973d95ea863
Joshua Ross <josh@rossjm.dev>
2024-07-07 22:07:55 -0400

    Create install.sh

---
+++ linux/cloudflared/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Author: Joshua Ross
+# Github: https://github.com/ColoredBytes
+# Purpose: Cloudflared Install Script
+
+
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared bookworm main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
