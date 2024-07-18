0b821b8a5936ec6057cbc126d2ac19b5bec8856e
Joshua Ross <josh@rossjm.dev>
2024-07-17 20:48:14 -0400

    changed the extract_pfx script up

--- scripts/ssl/extract_pfx.sh
+++ scripts/ssl/extract_pfx.sh
@@ -3,15 +3,10 @@
 # Github: https://github.com/ColoredBytes 
 # Purpose: pfx extraction script
 
-# Define the application name and the current date
-appName="YourAppName"  # Replace with your actual application name
+# Variables
+appName="gitea"  # Replace with your actual application name
 currentDate=$(date +%Y-%m-%d)
-
-# Display the application name and the current date
-echo "Application: $appName"
-echo "Date: $currentDate"
-
-# Define the paths for the input PFX file and the base output directory
+pfxFile="./pfx/gitea.pfx"  # Replace with the actual path to your PFX file
 outputDir="./$appName"
 
 # Ensure the output directory exists
@@ -23,20 +18,20 @@ fi
 read -s -p "Enter the PFX password: " pfxPassword
 echo
 
-# Define the path to the PFX file (modify this as needed)
-pfxFilePath="path/to/your/file.pfx"  # Replace with the actual path to your PFX file
+# Generate filenames with app name and date
+privKeyFile="${outputDir}/${appName}-${currentDate}-priv.key"
+pubCrtFile="${outputDir}/${appName}-${currentDate}-pub.crt"
+caCrtFile="${outputDir}/${appName}-${currentDate}-ca.crt"
 
-# Extract private key
-openssl pkcs12 -in "$pfxFilePath" -nocerts -nodes -out "$outputDir/key-pfx.pem" -passin pass:"$pfxPassword"
-# Extract client certificate
-openssl pkcs12 -in "$pfxFilePath" -clcerts -nokeys -out "$outputDir/cert-pfx.pem" -passin pass:"$pfxPassword"
-# Extract CA certificate chain
-openssl pkcs12 -in "$pfxFilePath" -cacerts -nokeys -chain -out "$outputDir/ca-pfx.pem" -passin pass:"$pfxPassword"
-# Convert private key to PEM format
-openssl rsa -in "$outputDir/key-pfx.pem" -out "$outputDir/key.pem"
-# Convert client certificate to PEM format
-openssl x509 -in "$outputDir/cert-pfx.pem" -out "$outputDir/cert.pem"
-# Convert CA certificate chain to PEM format
-openssl x509 -in "$outputDir/ca-pfx.pem" -out "$outputDir/ca.pem"
+# Extract the private key
+openssl pkcs12 -in "$pfxFile" -nocerts -nodes -out "$privKeyFile" -passin pass:"$pfxPassword"
+# Extract the public key
+openssl pkcs12 -in "$pfxFile" -clcerts -nokeys -out "$pubCrtFile" -passin pass:"$pfxPassword"
+# Extract the CA cert chain
+openssl pkcs12 -in "$pfxFile" -cacerts -nokeys -chain -out "$caCrtFile" -passin pass:"$pfxPassword"
+
+echo "Private key saved to: $privKeyFile"
+echo "Public key saved to: $pubCrtFile"
+echo "CA cert chain saved to: $caCrtFile"
 
 echo "Extraction and conversion completed."
